2c7bb7000627c7b7c4ddabd0b8555c38544dc184
Skalypto <80338702+SkalyptoSama@users.noreply.github.com>
2024-04-30 09:49:20 +0200

    Delete Sync.bat

--- Sync.bat
+++
@@ -1,8 +0,0 @@
-::This file goes in your USB drive
-
-:: Sets the source drive letter or path you want to backup
-SET SOURCE=%CD:~0,3%
-:: Sets the backup folder path
-SET BACKUP=D:\Proton Drive\jacquelin.gregoire\My files\testbackup
-:: You can edit the argusments of xcopy but this should only copy the difference between the source and the destination
-xcopy "%source%" "%backup%" /s /c /d /e /h /i /r /y
